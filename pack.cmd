ad8f7560ca1d4138cae51cc7294e63280331941e
Jon Canning <jon.canning@gmail.com>
2017-07-12 22:28:50 +0000

    pack.cmd

---
+++ pack.cmd
@@ -0,0 +1,3 @@
+SET NUGETVERSION=1.0.3
+dotnet pack Serilog.Sinks.Scalyr -c Release /p:PackageVersion=%NUGETVERSION%
+dotnet nuget push Serilog.Sinks.Scalyr\bin\Release\Serilog.Sinks.Scalyr.%NUGETVERSION%.nupkg -k %NUGETKEY% -s nuget.org
