b005b7ec215ece35f3bb5a3a0a684fad981447f0
Jon Canning <jon.canning@gmail.com>
2017-08-27 18:34:13 +0100

    netstandard 2.0

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 dotnet restore && \
 dotnet publish Serilog.Sinks.Scalyr.Tests && \
-dotnet Serilog.Sinks.Scalyr.Tests/bin/Debug/netcoreapp1.1/publish/Serilog.Sinks.Scalyr.Tests.dll
+dotnet Serilog.Sinks.Scalyr.Tests/bin/Debug/netcoreapp2.0/publish/Serilog.Sinks.Scalyr.Tests.dll
