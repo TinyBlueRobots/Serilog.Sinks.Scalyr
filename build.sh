14cd0a30cc1672baed1feb87ada2e81d9c050b81
Jon Canning <jon.canning@gmail.com>
2017-09-14 11:32:21 +0100

    tidy up

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+export FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.5/
 dotnet restore && \
 dotnet publish Serilog.Sinks.Scalyr.Tests && \
-dotnet Serilog.Sinks.Scalyr.Tests/bin/Debug/netcoreapp2.0/publish/Serilog.Sinks.Scalyr.Tests.dll
+dotnet Serilog.Sinks.Scalyr.Tests/bin/Debug/netcoreapp20/publish/Serilog.Sinks.Scalyr.Tests.dll
