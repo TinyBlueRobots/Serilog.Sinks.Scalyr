3e7acf0ac2719b9bb8b3d331ada887eb60aa7144
Logan Laughlin <loganlaughlin@gmail.com>
2022-02-01 15:19:17 -0600

    Patch SystemTextJsonScalyrFormatter to handle serializing the TargetSite property on Exceptions

---
+++ Serilog.Sinks.Scalyr/MethodBaseConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Serilog.Sinks.Scalyr
+{
+  /// <inheritdoc />
+  /// <summary>
+  ///   Handles properly converting MethodBase objects to Json
+  /// </summary>
+  internal class MethodBaseConverter : JsonConverter<MethodBase>
+  {
+    /// <inheritdoc />
+    public override MethodBase Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+      throw new NotSupportedException("Deserializing RuntimeMethodHandles is not allowed");
+
+    /// <inheritdoc />
+    public override void Write(Utf8JsonWriter writer, MethodBase value, JsonSerializerOptions options)
+    {
+      writer.WriteStartObject();
+
+      WriteProperty("Name", value.Name);
+      WriteProperty("DeclaringType", value.DeclaringType?.FullName);
+      WriteProperty("Description", value.ToString());
+
+      writer.WriteEndObject();
+
+      void WriteProperty(string name, string propertyValue)
+      {
+        switch (options)
+        {
+          case { DefaultIgnoreCondition: JsonIgnoreCondition.WhenWritingNull }
+            or { IgnoreNullValues: true }
+            when propertyValue is null:
+          case { DefaultIgnoreCondition: JsonIgnoreCondition.WhenWritingDefault }
+            when propertyValue == default:
+            return;
+        }
+
+        writer.WritePropertyName(name);
+        writer.WriteStringValue(propertyValue);
+      }
+    }
+  }
+}
